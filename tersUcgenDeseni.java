d67f7408e3fc72af40084eede65efa0952c2e594
Sumeyye Coskun <62007900+smyy96@users.noreply.github.com>
2022-01-26 16:58:04 +0300

    Create tersUcgenDeseni.java
    Ters Üçgen Yapımı.

---
+++ tersUcgenDeseni.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class tersUcgenDeseni {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+        
+        for (int i = n; i > 0; i--) {
+            for (int j = n+1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (i * 2 - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
