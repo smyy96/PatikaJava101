149afc58c590584d5e65ec3afe4bb5e175ea2c61
Sumeyye Coskun <62007900+smyy96@users.noreply.github.com>
2022-01-26 13:48:26 +0300

    Create ustluSayi.java
    Üslü sayı hesaplayan program.

---
+++ ustluSayi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ustluSayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, r,sum=1;
+        System.out.print("Sayi : ");
+        n = input.nextInt();
+        System.out.print("Üst : ");
+        r = input.nextInt();
+        for(int i=0;i<r;i++) {
+            sum*=n;
+        }
+        System.out.println(n + " üstü " + r + " = " +sum);
+    }
+}
