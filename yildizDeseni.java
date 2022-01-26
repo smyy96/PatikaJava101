cd771e99092532f85cc519bec2dcc0e7c291cec5
Sumeyye Coskun <62007900+smyy96@users.noreply.github.com>
2022-01-26 14:40:25 +0300

    Create yildizDeseni.java
    Yıldızlar ile elmas deseni.

---
+++ yildizDeseni.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class yildizDeseni {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i > 0; i--) {
+            for (int j = n+1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (i * 2 - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
+
