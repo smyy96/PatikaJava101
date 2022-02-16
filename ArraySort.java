d38769118dd0cfdcf15d0756f089b43ba48ba72f
Sumeyye Coskun <62007900+smyy96@users.noreply.github.com>
2022-02-16 21:51:35 +0300

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class ArraySort {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] num= new int[0];
         int arr;
         System.out.print("Dizinin boyutu n : ");
         int n=scan.nextInt();
+        int[] num= new int[n];
         System.out.println("Dizinin elemanlarını giriniz ");
-        int[] newarr=Arrays.copyOf(num,n);
         for (int i=0;i<n;i++){
             System.out.print((i+1)+". Elemanı : ");
             arr=scan.nextInt();
-            newarr[i]=arr;
+            num[i]=arr;
         }
         Arrays.sort(newarr);
         System.out.print("Sıralama : ");
-        for (int i:newarr) {
+        for (int i:num) {
             System.out.print(i +" ");
         }
 
